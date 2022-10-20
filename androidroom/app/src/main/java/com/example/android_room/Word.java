95d5339d0db4f48587ee057fddd1767791fd266a
Rosandymaulana <rosandy1118@gmail.com>
2022-10-20 18:32:01 +0700

    Task 9: Create an Adapter and adding the RecyclerView

--- androidroom/app/src/main/java/com/example/android_room/Word.java
+++ androidroom/app/src/main/java/com/example/android_room/Word.java
@@ -1,9 +1,9 @@
 package com.example.android_room;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "word_table")
 public class Word {
